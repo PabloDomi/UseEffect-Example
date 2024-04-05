457e6c3f1d22e82f64db6a8ff53a4ef028b58cde
pdomi <domi@usal.es>
2024-04-05 12:39:25 +0200

    component MonumentList

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Monument } from './types.d'
+import { MonumentsList } from './Components/MonumentsList';
 
 function App() {
 
-  const [monuments, setMonuments] = useState<Monument[]>([]);
-  
-  useEffect(() => {
-    fetch('https://monuments.milicia.net/monuments/provincia/Salamanca')
-      .then(res => res.json())
-      .then(data => setMonuments(data))
-  }, []);
-
   return (
     <div className="App">
-      <h1>Monumentos de Salamanca</h1>
-      <div>{monuments.map((monument, index) => <ul key={index}>{monument.nombre}</ul>)}</div>
+      <MonumentsList/>
     </div>
   );
 }
--- src/Components/MonumentsList.tsx
+++ src/Components/MonumentsList.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Monument } from '../Utils/types'
+
 export function MonumentsList() {
 
-    return (<h1>Monuments List</h1>)
+    const [monuments, setMonuments] = useState<Monument[]>([]);
+  
+  useEffect(() => {
+    fetch('https://monuments.milicia.net/monuments/provincia/Salamanca')
+      .then(res => res.json())
+      .then(data => setMonuments(data))
+  }, []);
+
+    return (
+    <div>
+        <h1>Monumentos de Salamanca</h1>
+        <div>
+            {monuments.map((monument, index) => (
+                <div key={monument.id}>
+                    <h2>{monument.nombre}</h2>
+                    <p>{monument.descripcion}</p>
+                    <p><strong>{monument.tipoMonumento}</strong></p>
+                </div>
+            ))}</div>
+    </div>
+    );   
 }
